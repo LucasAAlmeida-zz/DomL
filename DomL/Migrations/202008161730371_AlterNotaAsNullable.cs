71c04b8a68ef19c1315bbcd96318803260fda4bf
Lucas Augusto de Almeida <lucas.augusto.almeida@gmail.com>
2020-08-16 15:17:09 -0300

    migration alter nota as nullable

---
+++ DomL/Migrations/202008161730371_AlterNotaAsNullable.cs
@@ -0,0 +1,26 @@
+﻿namespace DomL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AlterNotaAsNullable : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Book", "Nota", c => c.Int());
+            AlterColumn("dbo.Comic", "Nota", c => c.Int());
+            AlterColumn("dbo.Game", "Nota", c => c.Int());
+            AlterColumn("dbo.Series", "Nota", c => c.Int());
+            AlterColumn("dbo.Watch", "Nota", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Watch", "Nota", c => c.Int(nullable: false));
+            AlterColumn("dbo.Series", "Nota", c => c.Int(nullable: false));
+            AlterColumn("dbo.Game", "Nota", c => c.Int(nullable: false));
+            AlterColumn("dbo.Comic", "Nota", c => c.Int(nullable: false));
+            AlterColumn("dbo.Book", "Nota", c => c.Int(nullable: false));
+        }
+    }
+}
